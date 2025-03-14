e7c84ccef82adb29b46b71c9ccbe447e4bdbe467
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-14 06:44:12 +0000

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
+  modules: ["@nuxt/content"],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: '2024-04-03',
-})
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+      dev: true,
+    },
+  },
+  compatibilityDate: "2024-04-03",
+});
