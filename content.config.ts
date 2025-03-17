46a70a4e959babf4ad9a05cfa30bdb9c3fd4a2fe
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-17 08:45:38 +0000

    fix

--- content.config.ts
+++ content.config.ts
@@ -6,14 +6,31 @@ export default defineContentConfig({
       type: "page",
       source: "**",
     }),
-    blog: defineCollection({
-      source: "blog/*.md",
+    posts: defineCollection({
       type: "page",
-      // Define custom schema for docs collection
+      source: "blog/*.md",
       schema: z.object({
-        tags: z.array(z.string()),
-        image: z.string(),
+        draft: z.boolean().default(false),
+        category: z.enum(["Alps", "Himalaya", "Pyrenees"]).optional(),
         date: z.date(),
+        image: z.object({
+          src: z.string().editor({ input: "media" }),
+          alt: z.string(),
+        }),
+        slug: z.string().editor({ hidden: true }),
+        icon: z.string().optional().editor({ input: "icon" }),
+        authors: z.array(
+          z.object({
+            slug: z.string(),
+            username: z.string(),
+            name: z.string(),
+            to: z.string(),
+            avatar: z.object({
+              src: z.string(),
+              alt: z.string(),
+            }),
+          })
+        ),
       }),
     }),
   },
