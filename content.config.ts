6c7b18addbe75f4417e0f8a4156c75c8b581a70a
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-17 08:29:58 +0000

    add blog example

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,20 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from "@nuxt/content";
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
-      type: 'page',
-      source: '**',
+      type: "page",
+      source: "**",
+    }),
+    blog: defineCollection({
+      source: "blog/*.md",
+      type: "page",
+      // Define custom schema for docs collection
+      schema: z.object({
+        tags: z.array(z.string()),
+        image: z.string(),
+        date: z.date(),
+      }),
     }),
   },
-})
+});
