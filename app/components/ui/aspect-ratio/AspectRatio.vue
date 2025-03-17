4fa4b2cef423bcc7f3aead8e55e172403e129506
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-17 08:14:55 +0000

    add aspect-ratio

---
+++ app/components/ui/aspect-ratio/AspectRatio.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { AspectRatioProps } from ".";
+
+const props = defineProps<AspectRatioProps>();
+</script>
+
+<template>
+  <AspectRatio v-bind="props">
+    <slot>
+      <img :src="props.src" :alt="props.alt" />
+    </slot>
+  </AspectRatio>
+</template>
---
+++ app/components/ui/aspect-ratio/index.ts
@@ -0,0 +1,6 @@
+import { type AspectRatioProps as RekaAspectRatioProps } from "reka-ui";
+
+export interface AspectRatioProps extends RekaAspectRatioProps {
+  src: string;
+  alt?: string;
+}
