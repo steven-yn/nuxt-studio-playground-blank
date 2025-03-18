b84611528011578460316338ac2dad1e18330f46
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-18 02:23:06 +0000

    fix

--- content.config.ts
+++ content.config.ts
@@ -1,33 +1,44 @@
-import { defineContentConfig, defineCollection, z } from "@nuxt/content";
+import { defineCollection, z } from "@nuxt/content";
 
-export default defineContentConfig({
-  collections: {
-    content: defineCollection({
-      type: "page",
-      source: "**",
-      schema: z.object({
-        draft: z.boolean().default(false),
-        category: z.enum(["Alps", "Himalaya", "Pyrenees"]).optional(),
-        date: z.date(),
-        image: z.object({
-          src: z.string().editor({ input: "media" }),
-          alt: z.string(),
-        }),
-        slug: z.string().editor({ hidden: true }),
-        icon: z.string().optional().editor({ input: "icon" }),
-        authors: z.array(
-          z.object({
-            slug: z.string(),
-            username: z.string(),
-            name: z.string(),
-            to: z.string(),
-            avatar: z.object({
-              src: z.string(),
-              alt: z.string(),
-            }),
-          })
-        ),
-      }),
-    }),
-  },
+const commonContentSchema = z.object({
+  title: z.string().nonempty(),
+  description: z.string().nonempty(),
+  date: z.string().nonempty(),
 });
+
+const commonBlogSchema = z.object({
+  draft: z.boolean().default(false),
+  category: z.enum(["Alps", "Himalaya", "Pyrenees"]).optional(),
+  date: z.date(),
+  image: z.object({
+    src: z.string().editor({ input: "media" }),
+    alt: z.string(),
+  }),
+  slug: z.string().editor({ hidden: true }),
+  icon: z.string().optional().editor({ input: "icon" }),
+  authors: z.array(
+    z.object({
+      slug: z.string(),
+      username: z.string(),
+      name: z.string(),
+      to: z.string(),
+      avatar: z.object({
+        src: z.string(),
+        alt: z.string(),
+      }),
+    })
+  ),
+});
+
+export const collections = {
+  content: defineCollection({
+    type: "page",
+    source: "**",
+    schema: commonContentSchema,
+  }),
+  blog: defineCollection({
+    type: "page",
+    source: "**",
+    schema: commonBlogSchema,
+  }),
+};
