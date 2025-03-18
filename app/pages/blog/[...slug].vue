3720bfb4678a0809cd602adbaafb52e5f76ff7f9
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-18 02:29:59 +0000

    fix

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { withLeadingSlash, joinURL } from "ufo";
+import type { Collections } from "@nuxt/content";
+
 const route = useRoute();
 
-const { data: page } = await useAsyncData("page-" + route.path, () => {
-  return queryCollection("blog").path(route.path).first();
-});
+const slug = computed(() =>
+  Array.isArray(route.params.slug)
+    ? (route.params.slug as string[])
+    : [route.params.slug as string]
+);
+const path = computed(() =>
+  withLeadingSlash(joinURL("", "blog", ...slug.value))
+);
+const collection = computed(() => `blog` as keyof Collections);
+
+const { data: page } = await useAsyncData(
+  path.value,
+  async () =>
+    (await queryCollection(collection.value)
+      .path(path.value)
+      .first()) as Collections["blog"]
+);
+
+if (!page.value)
+  throw createError({ statusCode: 404, statusMessage: "Page not found" });
 </script>
 
 <template>
