9ef5a4aee7243884ce34903b076a3ba5c5487a28
sungyeon.yoon <sungyeon.yoon@datamaker.io>
2025-03-18 02:11:47 +0000

    fix

--- content.config.ts
+++ content.config.ts
@@ -1,65 +1,43 @@
 import { defineContentConfig, defineCollection, z } from "@nuxt/content";
-import { asSeoCollection } from '@nuxtjs/seo/content'
-
+import { asSeoCollection } from "@nuxtjs/seo/content";
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: "page",
       source: "**",
-      schema: z.object({
-        draft: z.boolean().default(false),
-        category: z.enum(["Alps", "Himalaya", "Pyrenees"]).optional(),
-        date: z.date(),
-        image: z.object({
-          src: z.string().editor({ input: "media" }),
-          alt: z.string(),
-        }),
-        slug: z.string().editor({ hidden: true }),
-        icon: z.string().optional().editor({ input: "icon" }),
-        authors: z.array(
-          z.object({
-            slug: z.string(),
-            username: z.string(),
-            name: z.string(),
-            to: z.string(),
-            avatar: z.object({
-              src: z.string(),
-              alt: z.string(),
-            }),
-          })
-        ),
-      }),
     }),
-    blog: defineCollection(asSeoCollection({
-      type: "page",
-      source: {
-        include: "blog/*.md",
-        prefix: "/blog",
-      },
-      schema: z.object({
-        draft: z.boolean().default(false),
-        category: z.enum(["Alps", "Himalaya", "Pyrenees"]).optional(),
-        date: z.date(),
-        image: z.object({
-          src: z.string().editor({ input: "media" }),
-          alt: z.string(),
+    blog: defineCollection(
+      asSeoCollection({
+        type: "page",
+        source: {
+          include: "blog/*.md",
+          prefix: "/blog",
+        },
+        schema: z.object({
+          draft: z.boolean().default(false),
+          category: z.enum(["Alps", "Himalaya", "Pyrenees"]).optional(),
+          date: z.date(),
+          image: z.object({
+            src: z.string().editor({ input: "media" }),
+            alt: z.string(),
+          }),
+          slug: z.string().editor({ hidden: true }),
+          icon: z.string().optional().editor({ input: "icon" }),
+          authors: z.array(
+            z.object({
+              slug: z.string(),
+              username: z.string(),
+              name: z.string(),
+              to: z.string(),
+              avatar: z.object({
+                src: z.string(),
+                alt: z.string(),
+              }),
+            })
+          ),
         }),
-        slug: z.string().editor({ hidden: true }),
-        icon: z.string().optional().editor({ input: "icon" }),
-        authors: z.array(
-          z.object({
-            slug: z.string(),
-            username: z.string(),
-            name: z.string(),
-            to: z.string(),
-            avatar: z.object({
-              src: z.string(),
-              alt: z.string(),
-            }),
-          })
-        ),
-      }),
-    })),
+      })
+    ),
   },
 });
